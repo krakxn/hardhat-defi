388c89cce6132ade97cc1263b9281f8331ded8ea
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 14:13:13 +0530

    Update getWeth.js

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -5,14 +5,17 @@ const AMOUNT = ethers.utils.parseEther("0.1")
 
 async function getWeth() {
     const { deployer } = await getNamedAccounts()
+    
     const iWeth = await ethers.getContractAt(
         "IWeth",
         networkConfig[network.config.chainId].wethToken,
         deployer
     )
+    
     const txResponse = await iWeth.deposit({
         value: AMOUNT,
     })
+    
     await txResponse.wait(1)
     const wethBalance = await iWeth.balanceOf(deployer)
     console.log(`Got ${wethBalance.toString()} WETH`)

e43ba4cee2dd9ec7e0ba7415ebb66ec225d40862
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 14:14:54 +0530

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,3 +1,5 @@
+// Pricefeed addresses can be obtained at https://docs.chain.link/docs/reference-contracts
+
 const networkConfig = {
     31337: {
         name: "localhost",
@@ -6,11 +8,9 @@ const networkConfig = {
         daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
         daiToken: "0x6b175474e89094c44da98b954eedeac495271d0f",
     },
-    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
-    // Default one is ETH/USD contract on Kovan
     42: {
         name: "kovan",
-        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331", // Default one is ETH / USD contract on Kovan
         wethToken: "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
         lendingPoolAddressesProvider: "0x88757f2f99175387aB4C6a4b3067c77A695b0349",
         daiEthPriceFeed: "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541",
